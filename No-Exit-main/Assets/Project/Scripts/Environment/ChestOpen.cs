9c4e3b16593595ff2c71988f5f160eb395506882
Reem Rashed Husain Moosa Mohamed Albalooshi <202302093@student.polytechnic.bh>
2026-04-23 17:51:50 +0300

    initial commit

---
+++ No-Exit-main/Assets/Project/Scripts/Environment/ChestOpen.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class ChestOpen : MonoBehaviour
+{
+    public Transform lid;
+    public float openAngle = -90f;
+    public float speed = 2f;
+
+    public AudioClip openSound;
+    public AudioClip closeSound;
+
+    private AudioSource audioSource;
+    private bool isOpen = false;
+
+    private Quaternion closedRotation;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        closedRotation = lid.localRotation;
+    }
+
+    void Update()
+    {
+        Quaternion targetRotation;
+
+        if (isOpen)
+        {
+            targetRotation = Quaternion.Euler(openAngle, 0, 0);
+        }
+        else
+        {
+            targetRotation = closedRotation;
+        }
+
+        lid.localRotation = Quaternion.Slerp(
+            lid.localRotation,
+            targetRotation,
+            Time.deltaTime * speed);
+    }
+
+    public void OpenChest()
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+            audioSource.PlayOneShot(openSound);
+        }
+    }
+
+    public void CloseChest()
+    {
+        if (isOpen)
+        {
+            isOpen = false;
+            audioSource.PlayOneShot(closeSound);
+        }
+    }
+}
---
+++ No-Exit-main/Assets/Project/Scripts/Environment/SimpleRotate.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SimpleRotate : MonoBehaviour
+{
+    public float rotationSpeed = 50f;
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
