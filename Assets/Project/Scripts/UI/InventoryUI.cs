33dc22d4657464ec1ca31c2ce0f1dbebb5c95062
MaryamTalal105 <202302082@student.polytechnic.bh>
2026-05-10 20:40:09 +0300

    Added functional inventory UI and item interaction system

--- Assets/Project/Scripts/UI/InventoryUI.cs
+++ Assets/Project/Scripts/UI/InventoryUI.cs
@@ -3,17 +3,53 @@ using UnityEngine;
 public class InventoryUI : MonoBehaviour
 {
     public GameObject journalUI;
+    public GameObject blurOverlay;
+    public GameObject inventoryHUD;
+
+    public AudioSource paperSound;
+
+    // PLAYER
+    public MonoBehaviour playerController;
+    public MouseLook mouseLook;
 
     void Start()
     {
         journalUI.SetActive(false);
+        blurOverlay.SetActive(false);
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.I))
         {
-            journalUI.SetActive(!journalUI.activeSelf);
+            bool isOpen = !journalUI.activeSelf;
+
+            // UI
+            journalUI.SetActive(isOpen);
+            blurOverlay.SetActive(isOpen);
+            blurOverlay.SetActive(isOpen);
+
+            // SOUND
+            paperSound.Play();
+
+            // FREEZE PLAYER
+            playerController.enabled = !isOpen;
+            mouseLook.enabled = !isOpen;
+
+            // CURSOR
+            Cursor.visible = isOpen;
+
+            if (isOpen)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
         }
     }
 }
